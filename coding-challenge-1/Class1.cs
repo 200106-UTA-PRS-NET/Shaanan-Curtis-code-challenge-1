2c39f426e1161a8b4f8dfad87ec81c88b7697e69
redrhythm <shaanancurtis@ieee.org>
2020-01-13 14:30:09 -0600

    Library was somehow not included in last push so sending corrected version

---
+++ coding-challenge-1/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Library
+{
+    using System;
+
+    namespace Library
+    {
+        public class Class1
+        {
+            public bool Method1(string temp)
+            {
+                int i = 0;
+                int j = temp.Length - 1;
+                while (i < temp.Length / 2)
+                {
+                    if (!char.IsLetter(temp[i]))
+                        i++;
+                    else if (!char.IsLetter(temp[j]))
+                        j--;
+
+                    if (char.ToLower(temp[i]) != char.ToLower(temp[j]))
+                        return false;
+
+                    j--;
+                    i++;
+                }
+                return true;
+            }
+        }
+    }
+}
